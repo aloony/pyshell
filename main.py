95170b9c0619d4ecf049419ac9b71ce1eabace43
aloony <npp3rzwi@gmail.com>
2026-06-14 16:29:43 +0200

    Made a proper basic shell

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
+
 import subprocess as sp
+import readline
 import os
+import sys
 from contextlib import suppress as suppress_exception
 from utils import *
 from pathlib import Path
@@ -8,18 +11,30 @@ from pyparsing import (
     Word,
     ParseResults,
     alphas,
+    Literal,
     Keyword,
     StringStart,
     alphanums,
     OneOrMore,
     Optional,
     QuotedString,
+    printables,
     Combine,
     ZeroOrMore,
     Group,
     Suppress,
     LineEnd,
 )
+
+readline.parse_and_bind('"\\C-l": clear-screen')
+
+RESET = "\033[0m"
+CYAN = "\033[36m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+DIM = "\033[2m"
+CLEAR_SCREEN = "\033[2J\033[H"
+CTRL_L = "\x0c"
 
 env = os.environ
 shell_env = {}
@@ -29,22 +44,69 @@ identifier = Word(alphas + "_", alphanums + "_")
 endline = Suppress(Keyword(";"))
 
 # Command
-command_arg = QuotedString('"') | QuotedString("'")
-command = Group(
-    StringStart() + identifier("name") + ZeroOrMore(command_arg)("args") + endline
-)("command")
-
+command_arg = (
+    QuotedString('"') | QuotedString("'") | Word(printables, exclude_chars=" ")
+)
+command = Group(StringStart() + identifier("name") + ZeroOrMore(command_arg)("args"))(
+    "command"
+)
 
 text = Path("./file.crap").read_text().strip()
 
+
+def print_stdout(s: str):
+    sys.stdout.write(s)
+    sys.stdout.flush()
+
+
+def prompt(last_return_code):
+    if not last_return_code:
+        msg = f"{CYAN}{os.getcwd()}{RESET}{DIM}:{RESET} "
+    else:
+        msg = (
+            f"{CYAN}{os.getcwd()}{RESET} {RED}[{last_return_code}]{RESET}{DIM}:{RESET} "
+        )
+    return msg
+
+
+def clear_screen():
+    print_stdout(CLEAR_SCREEN)
+
+
+def print_error(msg):
+    print(f"{RED}{msg}{RESET}")
+
+
+last_return_code: int = 0
+clear_screen()
 while True:
+    parsed = None
     try:
-        parsed, start, end = next(command.scan_string(text, max_matches=1))
-        text = text[end:]
+        line = input(prompt(last_return_code))
+        if not line:
+            break
+        parsed, start, end = next(command.scan_string(line, max_matches=1))
+        if parsed.command.name == "exit":
+            exit()
 
         cmd = [parsed.command.name, *parsed.command.args]
-        result = sp.run(cmd, capture_output=True)
-        inspect(result)
+        result = sp.run(
+            cmd,
+            capture_output=True,
+        )
+        last_return_code = result.returncode
+        output = (result.stdout or result.stderr).decode()
+        print_stdout(f"{output}")
 
-    except StopIteration:
-        exit()
+    except (FileNotFoundError, StopIteration):
+        last_return_code = 1
+        msg = None
+        if parsed:
+            msg = parsed.command.name
+        else:
+            msg = line
+        print_error(f"No such file or directory: {msg}")
+    except KeyboardInterrupt:
+        last_return_code = 0
+        print_stdout("\n")
+        continue
